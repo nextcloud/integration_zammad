8e8dc8b60e42fc7ca809a184610f15503de96740
Julien Veyssier <eneiluj@posteo.net>
2020-07-27 10:52:27 +0200

    use IClient instead of php's file_get_contents to make requests
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

--- lib/Service/ZammadAPIService.php
+++ lib/Service/ZammadAPIService.php
@@ -13,6 +13,7 @@ namespace OCA\Zammad\Service;
 
 use OCP\IL10N;
 use OCP\ILogger;
+use OCP\Http\Client\IClientService;
 
 class ZammadAPIService {
 
@@ -25,11 +26,14 @@ class ZammadAPIService {
     public function __construct (
         string $appName,
         ILogger $logger,
-        IL10N $l10n
+        IL10N $l10n,
+        IClientService $clientService
     ) {
         $this->appName = $appName;
         $this->l10n = $l10n;
         $this->logger = $logger;
+        $this->clientService = $clientService;
+        $this->client = $clientService->newClient();
     }
 
     public function getNotifications($url, $accessToken, $authType, $since = null) {
@@ -94,57 +98,63 @@ class ZammadAPIService {
 
     // authenticated request to get an image from zammad
     public function getZammadAvatar($image, $url, $accessToken, $authType) {
+        $url = $url . '/api/v1/users/image/' . $image;
         $authHeader = ($authType === 'access') ? 'Token token=' : 'Bearer ';
         $options = [
-            'http' => [
-                'header'  => 'Authorization: ' . $authHeader . $accessToken .
-                    "\r\nUser-Agent: Nextcloud Zammad integration",
-                'method' => 'GET',
+            'headers' => [
+                'Authorization'  => $authHeader . $accessToken,
+                'User-Agent' => 'Nextcloud Zammad integration',
             ]
         ];
-
-        $url = $url . '/api/v1/users/image/' . $image;
-        $context = stream_context_create($options);
-        return file_get_contents($url, false, $context);
+        return $this->client->get($url, $options)->getBody();
     }
 
     public function request($url, $accessToken, $authType, $endPoint, $params = [], $method = 'GET') {
         try {
+            $url = $url . '/api/v1/' . $endPoint;
             $authHeader = ($authType === 'access') ? 'Token token=' : 'Bearer ';
             $options = [
-                'http' => [
-                    'header'  => 'Authorization: ' . $authHeader . $accessToken .
-                        "\r\nUser-Agent: Nextcloud Zammad integration",
-                    'method' => $method,
+                'headers' => [
+                    'Authorization'  => $authHeader . $accessToken,
+                    'User-Agent' => 'Nextcloud Zammad integration',
                 ]
             ];
 
-            $url = $url . '/api/v1/' . $endPoint;
             if (count($params) > 0) {
-                // manage array parameters
-                $paramsContent = '';
-                foreach ($params as $key => $value) {
-                    if (is_array($value)) {
-                        foreach ($value as $oneArrayValue) {
-                            $paramsContent .= $key . '[]=' . urlencode($oneArrayValue) . '&';
-                        }
-                        unset($params[$key]);
-                    }
-                }
-                $paramsContent .= http_build_query($params);
                 if ($method === 'GET') {
+                    // manage array parameters
+                    $paramsContent = '';
+                    foreach ($params as $key => $value) {
+                        if (is_array($value)) {
+                            foreach ($value as $oneArrayValue) {
+                                $paramsContent .= $key . '[]=' . urlencode($oneArrayValue) . '&';
+                            }
+                            unset($params[$key]);
+                        }
+                    }
+                    $paramsContent .= http_build_query($params);
                     $url .= '?' . $paramsContent;
                 } else {
-                    $options['http']['content'] = $paramsContent;
+                    $options['body'] = $params;
                 }
             }
 
-            $context = stream_context_create($options);
-            $result = file_get_contents($url, false, $context);
-            if (!$result) {
+            if ($method === 'GET') {
+                $response = $this->client->get($url, $options);
+            } else if ($method === 'POST') {
+                $response = $this->client->post($url, $options);
+            } else if ($method === 'PUT') {
+                $response = $this->client->put($url, $options);
+            } else if ($method === 'DELETE') {
+                $response = $this->client->delete($url, $options);
+            }
+            $body = $response->getBody();
+            $respCode = $response->getStatusCode();
+
+            if ($respCode >= 400) {
                 return $this->l10n->t('Bad credentials');
             } else {
-                return json_decode($result, true);
+                return json_decode($body, true);
             }
         } catch (\Exception $e) {
             $this->logger->warning('Zammad API error : '.$e, array('app' => $this->appName));
