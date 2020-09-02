90fc8d26fc31d4b1ce4cd72c5f3f1d528656d1e5
Julien Veyssier <eneiluj@posteo.net>
2020-09-02 13:26:06 +0200

    fix image loading with new webpack config
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

--- webpack.js
+++ webpack.js
@@ -8,18 +8,10 @@ if (webpackConfig.entry && webpackConfig.entry.main) {
 }
 
 const config = {
-    entry: {
+	entry: {
 		personalSettings: path.join(__dirname, 'src', 'personalSettings.js'),
 		adminSettings: path.join(__dirname, 'src', 'adminSettings.js'),
 		dashboard: path.join(__dirname, 'src', 'dashboard.js'),
-    },
-	module: {
-		rules: [
-			{
-				test: /\.(png|jpg|gif|svg|woff|woff2|eot|ttf)$/,
-				loader: 'url-loader',
-			},
-		],
 	},
 }
 
