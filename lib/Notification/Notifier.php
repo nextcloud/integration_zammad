3adc2d2d5409a023fa4cdc073619a98c7714ee0c
Julien Veyssier <eneiluj@posteo.net>
2020-09-28 15:25:10 +0200

    closes #3 plural translation
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

--- lib/Notification/Notifier.php
+++ lib/Notification/Notifier.php
@@ -87,7 +87,8 @@ class Notifier implements INotifier {
 		switch ($notification->getSubject()) {
 		case 'new_open_tickets':
 			$p = $notification->getSubjectParameters();
-			$content = $l->t('You have %s open tickets in Zammad.', [$p['nbOpen'] ?? 0]);
+			$nbOpen = (int) ($p['nbOpen'] ?? 0);
+			$content = $l->n('You have %s open ticket in Zammad.', 'You have %s open tickets in Zammad.', $nbOpen, [$nbOpen]);
 
 			//$theme = $this->config->getUserValue($userId, 'accessibility', 'theme', '');
 			//$iconUrl = ($theme === 'dark')
