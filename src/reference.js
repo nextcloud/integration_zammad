1ddf699b0302b6453105837ba0529215bfcb0e65
Julien Veyssier <julien-nc@posteo.net>
2023-06-05 18:02:18 +0200

    do not use OC functions/vars anymore
    Signed-off-by: Julien Veyssier <julien-nc@posteo.net>

--- src/reference.js
+++ src/reference.js
@@ -23,9 +23,11 @@
 import {} from '@nextcloud/vue-richtext'
 
 import { registerWidget } from '@nextcloud/vue/dist/Components/NcRichText.js'
+import { linkTo } from '@nextcloud/router'
+import { getRequestToken } from '@nextcloud/auth'
 
-__webpack_nonce__ = btoa(OC.requestToken) // eslint-disable-line
-__webpack_public_path__ = OC.linkTo('integration_zammad', 'js/') // eslint-disable-line
+__webpack_nonce__ = btoa(getRequestToken()) // eslint-disable-line
+__webpack_public_path__ = linkTo('integration_zammad', 'js/') // eslint-disable-line
 
 registerWidget('integration_zammad', async (el, { richObjectType, richObject, accessible }) => {
 	const { default: Vue } = await import(/* webpackChunkName: "reference-lazy" */'vue')
