7a04e9e8f0b947e237200811df743d6a7972b8dd
Julien Veyssier <julien-nc@posteo.net>
2023-06-05 18:04:03 +0200

    bump max nc version to 28, get rid of @nextcloud/vue-richtext
    Signed-off-by: Julien Veyssier <julien-nc@posteo.net>

--- src/reference.js
+++ src/reference.js
@@ -19,9 +19,6 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-// with nc/vue 7.8.0, if we remove this, nothing works...
-import {} from '@nextcloud/vue-richtext'
-
 import { registerWidget } from '@nextcloud/vue/dist/Components/NcRichText.js'
 import { linkTo } from '@nextcloud/router'
 import { getRequestToken } from '@nextcloud/auth'
